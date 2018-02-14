b32d7bd4e0b61135b2ddbaf88c354295501f4daa
Henrique Merlin <henmerlin@gmail.com>
2018-02-14 18:10:54 -0200

    update service cent

--- swarm/install-service-cent.sh
+++ swarm/install-service-cent.sh
@@ -1,4 +1,9 @@
 cp -R /opt/shellscripts/swarm/customerAPI-swarm-cent.service /etc/systemd/system/customerAPI-swarm-cent.service
 chmod 777 /etc/systemd/system/customerAPI-swarm-cent.service
+
+firewall-cmd --zone=dmz --add-port=7171/tcp --permanent
+firewall-cmd --zone=public --add-port=7171/tcp --permanent
+firewall-cmd --reload
+
 systemctl daemon-reload
 systemctl enable customerAPI-swarm-cent.service
