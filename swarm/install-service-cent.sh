a1f77f9d08a8e93db6c1bb4f62e47631efde86f9
Henrique Merlin <henmerlin@gmail.com>
2018-02-14 16:06:48 -0200

    update

--- swarm/install-service-cent.sh
+++ swarm/install-service-cent.sh
@@ -1,4 +1,4 @@
 chmod 755 /opt/shellscripts/swarm/customerAPI-swarm-cent.service
-cp -f /opt/shellscripts/swarm/customerAPI-swarm-cent.service /etc/systemd/system/multi-user.target.wants/customerAPI-swarm-cent.service
+cp /opt/shellscripts/swarm/customerAPI-swarm-cent.service /etc/systemd/system/multi-user.target.wants/customerAPI-swarm-cent.service --force
 systemctl daemon-reload
 systemctl enable customerAPI-swarm-cent.service
