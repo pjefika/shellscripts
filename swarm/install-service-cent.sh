b51a92c7d778e361690fb78afac9022a37bb7297
Henrique Merlin <henmerlin@gmail.com>
2018-02-22 10:29:03 -0300

    add  service

--- swarm/install-service-cent.sh
+++ swarm/install-service-cent.sh
@@ -1,9 +1,13 @@
-cp -R /opt/shellscripts/swarm/customerAPI-swarm-cent.service /etc/systemd/system/customerAPI-swarm-cent.service
-chmod 777 /etc/systemd/system/customerAPI-swarm-cent.service
 
-firewall-cmd --zone=dmz --add-port=7171/tcp --permanent
-firewall-cmd --zone=public --add-port=7171/tcp --permanent
+DEPLOY_NAME=$0
+PORT=$1
+
+cp -R /opt/shellscripts/swarm/$DEPLOY_NAME-swarm-cent.service /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
+chmod 777 /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
+
+firewall-cmd --zone=dmz --add-port=$PORT/tcp --permanent
+firewall-cmd --zone=public --add-port=$PORT/tcp --permanent
 firewall-cmd --reload
 
 systemctl daemon-reload
-systemctl enable customerAPI-swarm-cent.service
+systemctl enable $DEPLOY_NAME-swarm-cent.service
