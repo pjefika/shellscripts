3fdac042570bc433d4447863621632695be31653
Henrique Merlin <henmerlin@gmail.com>
2018-02-22 10:36:21 -0300

    update

--- swarm/install-service-cent.sh
+++ swarm/install-service-cent.sh
@@ -1,15 +1,14 @@
 
-DEPLOY_NAME=$0
-PORT=$1
+DEPLOY_NAME=$1
+PORT=$2
 
-echo $0 $1
 
-#cp -R /opt/shellscripts/swarm/$DEPLOY_NAME-swarm-cent.service /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
-#chmod 777 /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
+cp -R /opt/shellscripts/swarm/$DEPLOY_NAME-swarm-cent.service /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
+chmod 777 /etc/systemd/system/$DEPLOY_NAME-swarm-cent.service
 
-#firewall-cmd --zone=dmz --add-port=$PORT/tcp --permanent
-#firewall-cmd --zone=public --add-port=$PORT/tcp --permanent
-#firewall-cmd --reload
+firewall-cmd --zone=dmz --add-port=$PORT/tcp --permanent
+firewall-cmd --zone=public --add-port=$PORT/tcp --permanent
+firewall-cmd --reload
 
-#systemctl daemon-reload
-#systemctl enable $DEPLOY_NAME-swarm-cent.service
+systemctl daemon-reload
+systemctl enable $DEPLOY_NAME-swarm-cent.service
